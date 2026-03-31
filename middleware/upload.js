68591afe77cccf35d1f9411c4c5257f16ed8e67b
AsmitSharma2004 <asmitsharma2004@gmail.com>
2026-03-31 17:32:24 +0530

    added upload middleware

---
+++ middleware/upload.js
@@ -0,0 +1,41 @@
+import multer from "multer";
+import fs from "fs";
+
+const uploadDir = "uploads";
+
+// create uploads folder if not exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = [
+    "application/pdf",
+    "image/jpeg",
+    "image/png",
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF, JPG, PNG allowed"), false);
+  }
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+    files: 2,
+  },
+});
