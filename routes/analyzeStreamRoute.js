242dbc1c3235eea8dea155838733f817c6aa98da
AsmitSharma2004 <asmitsharma2004@gmail.com>
2026-03-31 17:21:00 +0530

    Streaming response

--- routes/analyzeStreamRoute.js
+++ routes/analyzeStreamRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { analyzeCaseStream } from "../controllers/analyzeStreamController.js";
+import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
