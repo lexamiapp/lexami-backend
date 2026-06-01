bd0a927b4e9c638b6876123614914d4f8c6d848f
LexAni Dev <pratyush@nyaymitra.in>
2026-06-01 15:11:16 +0200

    feat: update web app to latest version with all fixes

--- website/flutter_bootstrap.js
+++ website/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "200207682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "124312251" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
